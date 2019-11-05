a78d28d2f933443d48bc4354eb33bb3264bbe9a2
lechcirmirakis <lech.cirmirakis@go2.pl>
2019-11-05 12:57:29 +0100

    git add container hoc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from './hoc/container';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <Container>
+        
+      </Container>
     </div>
   );
 }
